6895eff48abc3fd8a7086f2d0c42fe44c6ef50c5
oddlyspaced <oddlyspaced1@gmail.com>
2020-06-24 16:06:03 +0530

    Update layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,104 @@ class NotesApp extends StatelessWidget {
   }
 }
 
+class NotesList extends StatefulWidget {
+  @override
+  _NotesListState createState() => _NotesListState();
+}
+
+/*
+ body: SingleChildScrollView(
+        child: Center(
+          child: Wrap(
+            alignment: WrapAlignment.center,
+            children: List.generate(100, (index) => index).map((e) {
+              bool check = (e + 1) % 3 == 0;
+              return Container(
+                decoration: BoxDecoration(
+                    color: check ? Colors.red : Colors.yellow,
+                    border: Border.all()),
+                height: height * 0.15,
+                width: check ? width : width * 0.5,
+                child: Column(
+                  children: [Text(e.toString()), Text((e * e * e).toString())],
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+      */
+
+class _NotesListState extends State<NotesList> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    List<Color> colors = [Color.fromARGB(255, 255, 172, 145), Color.fromARGB(255, 255, 204, 128), Color.fromARGB(255, 230, 238, 155), Color.fromARGB(255, 128, 122, 234), Color.fromARGB(255, 207, 147, 217), Color.fromARGB(255, 244, 143, 177), Color.fromARGB(255, 128, 203, 196)];
+    int color = 0;
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 37, 37, 37),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Wrap(
+              alignment: WrapAlignment.center,
+              children: List.generate(10, (index) => index).map((i) {
+                bool left = i % 2 == 0; // left = true, right = false
+
+                if (color == colors.length)
+                color = 0;
+
+                return Container(
+                width: width * 0.5,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  margin: EdgeInsets.only(
+                    left: (left)? 18 : 9,
+                    right: (left)? 9 : 18,
+                    bottom: 9,
+                    top: 9,
+                  ),
+                  color: colors[color++],
+                  elevation: 0,
+                  child: Padding(
+                    padding: EdgeInsets.all(18),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(
+                            bottom: 8
+                          ),
+                          child: Text(
+                            "How to make your personal brand stand out online",
+                            style: TextStyle(
+                              fontFamily: "GoogleSans",
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "May 21,2020",
+                          style: TextStyle(
+                            fontFamily: "GoogleSans",
+                            color: Color.fromARGB(100, 0, 0, 0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+              }).toList()),
+        ),
+      ),
+    );
+  }
+}
+
 class NotesHome extends StatefulWidget {
   @override
   _NotesHomeState createState() => _NotesHomeState();
@@ -34,82 +132,7 @@ class _NotesHomeState extends State<NotesHome> {
           ),
         ),
       ),
-      body: Container(
-        color: Color.fromARGB(255, 37, 37, 37),
-        child: Row(
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width / 2,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                margin: EdgeInsets.only(
-                  left: 18,
-                  right: 9,
-                  bottom: 9,
-                  top: 9,
-                ),
-                color: Color.fromARGB(255, 255, 171, 145),
-                elevation: 0,
-                child: Padding(
-                  padding: EdgeInsets.all(18),
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        "How to make your personal brand stand out online",
-                        style: TextStyle(
-                          fontFamily: "GoogleSans",
-                          fontSize: 18,
-                        ),
-                      ),
-                      Text("dd"),
-                      Text("dd"),
-                      Text("dd"),
-                      Text("dd"),
-
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            
-            
-            Container(
-              width: MediaQuery.of(context).size.width / 2,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                margin: EdgeInsets.only(
-                  left: 9,
-                  right: 18,
-                  bottom: 9,
-                  top: 9,
-                ),
-                color: Color.fromARGB(255, 255, 171, 145),
-                elevation: 0,
-                child: Padding(
-                  padding: EdgeInsets.all(18),
-                  child: Row(
-                    children: <Widget>[
-                      Flexible(
-                        child: Text(
-                          "How to make your personal brand stand out online",
-                          style: TextStyle(
-                            fontFamily: "GoogleSans",
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+      body: NotesList(),
     );
   }
 }
