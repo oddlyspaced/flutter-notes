9e5f2302b92fdfa135e3ff1b99ff2fc24f0a75d7
oddlyspaced <oddlyspaced1@gmail.com>
2020-06-27 14:10:24 +0530

    Add action button to editnote

--- lib/editnote.dart
+++ lib/editnote.dart
@@ -9,14 +9,27 @@ class _EditNoteState extends State<EditNote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: Key("_scaffold"),
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Scaffold.of(context).showSnackBar(SnackBar(content: Text("ss")));
-          },
+        actions: <Widget>[
+          Card(
+            color: Color.fromARGB(255, 59, 59, 59),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Container(
+              margin: EdgeInsets.fromLTRB(13.5, 9, 13.5, 9),
+              child: Icon(Icons.edit)
+            ),
+          )
+        ],
+        leading: Card(
+          color: Color.fromARGB(255, 59, 59, 59),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Icon(Icons.arrow_back_ios),
         ),
-        actions: <Widget>[],
         backgroundColor: Color.fromARGB(255, 37, 37, 37),
         elevation: 0,
       ),
@@ -36,50 +49,52 @@ class _EditNoteBodyState extends State<EditNoteBody> {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 37, 37, 37),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: width,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(18, 9, 18, 9),
-                child: Text(
-                  "Beautiful weather app UI concepts we wish existed",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: "GoogleSans",
-                      fontWeight: FontWeight.bold,
-                      fontSize: 32),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: width,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(18, 9, 18, 9),
+                  child: Text(
+                    "Beautiful weather app UI concepts we wish existed",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "GoogleSans",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32),
+                  ),
                 ),
               ),
-            ),
-            Container(
-              width: width,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(18, 9, 18, 9),
-                child: Text(
-                  "May 21, 2020",
-                  style: TextStyle(
-                      color: Colors.white24,
-                      fontFamily: "GoogleSans",
-                      fontSize: 20),
+              Container(
+                width: width,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(18, 9, 18, 9),
+                  child: Text(
+                    "May 21, 2020",
+                    style: TextStyle(
+                        color: Colors.white24,
+                        fontFamily: "GoogleSans",
+                        fontSize: 20),
+                  ),
                 ),
               ),
-            ),
-            Container(
-              width: width,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(18, 9, 18, 18),
-                child: Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  style: TextStyle(
-                      color: Colors.white70,
-                      fontFamily: "GoogleSans",
-                      fontSize: 20),
+              Container(
+                width: width,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(18, 9, 18, 18),
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    style: TextStyle(
+                        color: Colors.white70,
+                        fontFamily: "GoogleSans",
+                        fontSize: 20),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
